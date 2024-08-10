10ac36207bdc5310a7d9747e3bdcd4716d18d7d6
Subhash Ch. Pal <34298283+ShubhInfotech-Bhilai@users.noreply.github.com>
2024-08-10 17:24:21 +0530

    final commit

--- main_test.go
+++ main_test.go
@@ -15,3 +15,17 @@ func TestAdd(t *testing.T) {
         t.Errorf("Add(2, 3) = %d; want %d", result, expected)
     }    
 }
+
+func TestAdd1(t *testing.T) {
+    result := Add(2, 3)
+    expected := 5
+    if result != expected {
+        t.Errorf("Add(2, 3) = %d; want %d", result, expected)
+    }   
+    
+    result1 := Add(2, 3)
+    expected1 := 6
+    if result1 != expected1 {
+        t.Errorf("Add(2, 3) = %d; want %d", result, expected)
+    }    
+}
